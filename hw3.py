8a45a3dfaa130274d716dd70fef25fa418ea4c37
Kaitlynn Pham <kaitlynnpham@gmail.com>
2024-03-22 05:04:26 -0700

    add coefficient formula

--- hw3.py
+++ hw3.py
@@ -4,13 +4,21 @@
 
 import pandas as pd
 import numpy as np 
+from sklearn.model_selection import KFold
 
 data = pd.read_csv('auto-mpg.data.csv')
 
-
-y = data.iloc[:, 0] 
 X = data.drop('mpg', axis=1)
 X = X.drop('carname', axis=1)
+y = data.iloc[:, 0] 
 
 
 #add bias intercept(1) to input vector 
+bias = np.ones(X.shape[0], 1) 
+addBias_X = np.c_[bias, X]   
+
+# b = ((X^T(X))^-1)X^T(y) - use this formula to get coefficients
+
+coefficient = np.linalg.inv(addBias_X.T.dot(addBias_X)).dot(addBias_X).dot(y)
+#cross validation for 10 folds
+KFold(n_splits=10, random_state=None, shuffle=False)
